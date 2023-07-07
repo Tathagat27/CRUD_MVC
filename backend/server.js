0f73e13cb639d07ab5e5868144fb351ebcee4355
Tathagat27 <tat27302@gmail.com>
2023-07-08 05:11:55 +0530

    all the files mongodb, routes and the updated server.js has been added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
-const app = express()
+const connectdb = require("./mongodb");
 
-app.use(express.json())
-app.use(cors())
+const todoRoute = require("./routes/todoRoute");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectdb();
+
+app.use("/api", todoRoute);
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-})
+  console.log("Server is running on port 5000");
+});
