2d464c337110109befee6824a52b54527070d510
Tathagat27 <tat27302@gmail.com>
2023-08-24 19:35:10 +0530

    ignored files using .env

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectdb = () =>
   mongoose
-    .connect("mongodb://127.0.0.1:27017/todo_app", {
+    .connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
