92564030d0c767eb6b2dff6dda6cafa1082a6009
Tathagat27 <tat27302@gmail.com>
2023-08-27 15:41:32 +0530

    fixing difference in folders name

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 5000;
 // Your code
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
           if(err) {
               res.status(500).send(err)
           }
