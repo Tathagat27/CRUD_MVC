4f0720b7f08adfa66bcbb3c92b33f3d9159ee20f
Tathagat27 <tat27302@gmail.com>
2023-08-28 04:25:31 +0530

    env variables added

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api", todoRoute);
 
 // Your code
 if (process.env.NODE_ENV === "production") {
@@ -32,7 +33,6 @@ if (process.env.NODE_ENV === "production") {
 
 connectdb();
 
-app.use("/api", todoRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
