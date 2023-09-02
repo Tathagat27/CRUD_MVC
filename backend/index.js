700e229ddbdce6aacd37af46ca49770882791b00
Tathagat27 <tat27302@gmail.com>
2023-09-02 13:31:03 +0530

    database connection config

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+connectdb();
+
 app.use(express.json());
 app.use(cors());
 app.use("/api", todoRoute);
@@ -29,9 +31,6 @@ app.use("/api", todoRoute);
 //   })
 // }
 // Your code
-
-
-connectdb();
 
 
 app.listen(PORT, () => {
